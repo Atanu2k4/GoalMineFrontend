2d04945c03dbde69c0cdeef5c63ef658db3bed28
Atanu2k4 <atanu.saha2004@gmail.com>
2025-04-13 02:12:57 +0530

    Add Firebase configuration and authentication setup

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-G7bZT77_Dq_MKrP_nrQveCF-hBCOIXM",
+  authDomain: "goalmine-bcc41.firebaseapp.com",
+  projectId: "goalmine-bcc41",
+  storageBucket: "goalmine-bcc41.firebasestorage.app",
+  messagingSenderId: "711784131689",
+  appId: "1:711784131689:web:73dd5106b87f794961c6f8",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Auth with the app instance
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+// Configure Google provider
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export { auth, provider };
